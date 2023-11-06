b8feffee5620d5929137110018e36dcec513c7b7
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-06 19:48:53 +0200

    Implement infinite queries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,6 @@
 import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 
