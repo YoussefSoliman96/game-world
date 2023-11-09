39a86479d8270be9029458e8df4e1caa45154695
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-09 04:28:57 +0200

    Build game attributes

--- src/hooks/Game.ts
+++ src/hooks/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "../entities/Genre";
 import { Platform } from "../entities/Platform";
+import { Publisher } from "../entities/Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
