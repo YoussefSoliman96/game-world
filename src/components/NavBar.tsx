921e1ee09c7cce9b83a4c721f27b0483b37b6b9a
YoussefSoliman96 <youssef.soliman6636@gmail.com>
2023-11-09 05:52:18 +0200

    Clear input after search, clicking homepage icon resets results

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
+import useGameQueryStore from "../store";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
   return (
     <HStack padding="10px">
-      <Link to={"/"}>
+      <Link to={"/"} onClick={() => setSearchText("")}>
         <Image src={logo} boxSize="60px" objectFit={"cover"} />
       </Link>
       <SearchInput />
